c74572c0fba84cc94c41e87b6a147d7a059541fe
liuxianglin <lewislinlin@gmail.com>
2018-04-22 22:49:52 +0800

    代码组织与ES6模块

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+
+export const PATH_BASE = "https://hn.algolia.com/api/v1";
+export const PATH_SEARCH = '/search';
+export const PARAM_SEARCH = 'query=';
+export const PARAM_PAGE = 'page=';
+
+export const DEFAULT_QUERY = "aaa" //"redux"
+export const DEFAULT_HPP = '2'
+export const PARAM_HPP = 'hitsPerPage='
